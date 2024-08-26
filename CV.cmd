19cb83acbc33470339dc471a67a12141830d2631
MrR736 <mrr6451@outlook.com>
2024-08-26 09:05:06 -0700

    Add files via upload

--- CV.cmd
+++ CV.cmd
@@ -1,21 +1,8 @@
 @echo off
 set VERSION-CP=0.0.0.0.2
-set VERSION-CU=0.0.0.0.2
-set VERSION-CSe=0.0.0.0.1
-set VERSION-CSo=0.0.0.0.1
 
 echo Core Panel
 echo Version %VERSION-CP%
 echo:
 
-echo CoreUpdates
-echo Version %VERSION-CU%
-echo:
-
-echo CoreSettings
-echo Version %VERSION-CSe%
-echo:
-
-echo CoreSotfwares
-echo Version %VERSION-CSo%
-@echo on
+%Core%\CoreControls\CV.cmd
