cfd7b7207b9438b88a01883c9ea071313c872c98
MrR736 <mrr6451@outlook.com>
2024-09-03 17:39:24 -0700

    Update UpdateCore.bat

--- UpdateCore.bat
+++ UpdateCore.bat
@@ -4,28 +4,32 @@
 if /i "%1"=="" (
 	echo /A   Update All Of Core
 	echo /CC  Only CoreControls
+        echo /CCo Only CoreCommands
 	echo /CP  Only CorePrompt
 	echo /CSe Only CoreSettings
 	echo /SSo Only CoreSotfwares
 	echo /CU  Only CoreUpdates
 	exit /b
 ) else if /i "%1"=="/a" (
-    start call "%ProgramData%\PhoenixOS\Core\CoreCommands\UpdateCore\A.cmd"
+        start call "%ProgramData%\PhoenixOS\Core\CoreCommands\UpdateCore\A.cmd"
 	exit /b
 ) else if /i "%1"=="/cc" (
-    start call "%ProgramData%\PhoenixOS\Core\CoreCommands\UpdateCore\CC.cmd"
+        start call "%ProgramData%\PhoenixOS\Core\CoreCommands\UpdateCore\CC.cmd"
+	exit /b
+) else if /i "%1"=="/cco" (
+        start call "%ProgramData%\PhoenixOS\Core\CoreControls\CCo\CCo.cmd"
 	exit /b
 ) else if /i "%1"=="/cp" (
-    start call "%ProgramData%\PhoenixOS\Core\CoreCommands\UpdateCore\CP.cmd"
+        start call "%ProgramData%\PhoenixOS\Core\CoreCommands\UpdateCore\CP.cmd"
 	exit /b
 ) else if /i "%1"=="/cse" (
     start call "%ProgramData%\PhoenixOS\Core\CoreCommands\UpdateCore\CSe.cmd"
 	exit /b
 ) else if /i "%1"=="/cso" (
-    start call "%ProgramData%\PhoenixOS\Core\CoreCommands\UpdateCore\CSo.cmd"
+        start call "%ProgramData%\PhoenixOS\Core\CoreCommands\UpdateCore\CSo.cmd"
 	exit /b
 ) else if /i "%1"=="/cu" (
-    start call "%ProgramData%\PhoenixOS\Core\CoreCommands\UpdateCore\CU.cmd"
+        start call "%ProgramData%\PhoenixOS\Core\CoreCommands\UpdateCore\CU.cmd"
 	exit /b
 ) else (
     echo Unknown option %1
