2142c3e4bb985459e037bafbfd756301424cdb20
MrR736 <mrr6451@outlook.com>
2024-09-03 17:56:18 -0700

    Update Page.cmd

--- Page.cmd
+++ Page.cmd
@@ -1,4 +1,21 @@
 @echo off
-set page=https://github.com/MrR736/Core
-echo %page%
-@echo on
+
+:Page
+if /i "%1"=="" (
+    echo /M   Main Page
+	echo /D   Download Pages
+	exit /b
+) else if /i "%1"=="/m" (
+    echo https://github.com/MrR736
+    exit /b
+) else if /i "%1"=="/d" (
+    echo https://github.com/MrR736/Core/archive/refs/heads/main.zip
+    echo https://github.com/MrR736/CoreCommands/archive/refs/heads/main.zip
+    echo https://github.com/MrR736/CoreControls/archive/refs/heads/main.zip
+    echo https://github.com/MrR736/CoreSettings/archive/refs/heads/main.zip
+    echo https://github.com/MrR736/CoreUpdates/archive/refs/heads/main.zip
+    exit /b
+) else (
+    echo Unknown option %1
+)
+exit /b
