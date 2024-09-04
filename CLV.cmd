2ca48350a21ceb1a83d297c012b68fb0a2b3f5c1
MrR736 <mrr6451@outlook.com>
2024-09-04 16:28:49 -0700

    Update CLV.cmd

--- CLV.cmd
+++ CLV.cmd
@@ -1,12 +1,5 @@
 @echo off
-cd %core%\CoreControls\CV
-mkdir Temp
-start call %core%\CoreControls\CV\CheckLastVersion.cmd
-cls
-timeout /t 3 /nobreak
-start call %core%\CoreControls\CV\ExtraDel.cmd
-cd %core%\
-cls
-echo Latest Versions
+echo Core Latest Versions
 echo:
-%core%\CoreControls\CV\Temp\CV.cmd
+call %ProgramData%\PhoenixOS\Core\CoreControls\CV\CheckLastVersion.cmd
+cd %ProgramData%\PhoenixOS\Core
