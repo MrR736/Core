7effd4092109dbbcc05d0380f1bfe22bac997319
MrR736 <mrr6451@outlook.com>
2024-10-04 06:18:59 -0700

    Delete CoreSotfwares directory

--- CoreSotfwares/CV.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-set VERSION-CSo=0.0.0.0.4
-
-echo CoreSoftwares
-echo Version %VERSION-CSo%
--- CoreSotfwares/Setup.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d %~dp0
-for /f "tokens=*" %%f in ('dir /b "*.bat"') do (
-    @start /w /b cmd /c call "%%f"
-)
