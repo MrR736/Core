1d0519877cf4f53914b49d15895ea2b1beb12e60
MrR736 <mrr6451@outlook.com>
2024-10-04 07:08:02 -0700

    Add files via upload

---
+++ CoreHelp.bat
@@ -0,0 +1,24 @@
+@echo off
+set "cp=%ProgramData%\PhoenixOS\Core"
+:CoreHelp
+if /i "%1"=="" (
+	echo CoreCommands
+	echo  CLV             Check Last Version from Internet
+	echo  Core            Check Version
+	echo  CoreSettings    CoreSettings Enable or Disable Settings in Core
+	echo  Page            Show GitHub Page And Downloads Page
+	echo  Update          Update Software from Internet
+	echo  UpdateCore      Update Core from Internet
+) else if /i "%1"=="Core" (
+	echo 2
+	exit /b
+) else if /i "%1"=="CoreSettings" (
+	CoreSettings
+) else if /i "%1"=="Page" (
+	Page
+) else if /i "%1"=="UpdateCore" (
+	UpdateCore
+) else (
+	echo Unknown option %1
+)
+exit /b
